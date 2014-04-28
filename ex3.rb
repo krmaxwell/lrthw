2d1487e31471ae44c15682b9512da4e83e9ed888
Kyle Maxwell <kylem@xwell.org>
2014-04-27 20:19:26 -0500

    Use floating-point math for more accuracy

--- ex3.rb
+++ ex3.rb
@@ -5,7 +5,7 @@ puts "Roosters:", 100 - 25 * 3 % 4
 
 puts "Now I will count the eggs."
 
-puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1.0 / 4 + 6
 
 puts "Is it true that 3 + 2 < 5 - 7?"
 
